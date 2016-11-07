94178a44a63a008f81b6a8ade665445ec8f59c1d
usman-tahir <tahir.usman.ali94@outlook.com>
2016-11-07 12:36:19 -0500

    added an ANT project with JAR file capabilities

---
+++ ant-basics/sample-three/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+    public void execute() {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+    }
+}
