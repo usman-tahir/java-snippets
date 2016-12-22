1ae57eca5f783435804baa565d0ad8b46ae9c578
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-12-22 18:41:37 -0500

    created a basic Hello World Maven project

---
+++ maven-basics/my-app/src/main/java/com/utahir/app/App.java
@@ -0,0 +1,13 @@
+package com.utahir.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
